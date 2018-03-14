2baa7e9a8b1d9c5ce9ab93d6c7149528b2af3321
averonesis <averonesis@gmail.com>
2018-03-14 21:26:57 +0300

    8
    8M

--- js/index.js
+++ js/index.js
@@ -1,124 +1,40 @@
-var $text = $('.editor-text');
+var flowers = void 0;
+var frequency = 200;
 
-function set(txt) {
-  $text.html(txt);
+function startThisShit() {
+  flowers = setInterval(function () {
+    var flower = document.createElement('div');
+    flower.classList.add('flower');
+    flower.style.left = Math.random() * 100 + 'vw';
+    flower.style.top = Math.random() * 100 + 'vh';
+    flower.innerHTML = '🌸';
+    document.body.appendChild(flower);
+    flower.addEventListener('animationend', function () {
+      flower.remove();
+    });
+  }, frequency);
 }
 
-function slicer(str) {
-  var sliced = [];
-  for (var i = 0; i <= str.length; i++) {
-    sliced.push(str.slice(0, i));
+function stopThisShit() {
+  clearInterval(flowers);
+}
+
+var doc = document.body;
+var btn = document.querySelector('.js-btn');
+var range = document.querySelector('.js-range');
+
+doc.addEventListener('keydown', function (e) {
+  if (e.keyCode === 32 && e.repeat === false) {
+    startThisShit();
   }
-  
-  return sliced;
-}
-
-function pause (str, time) {
-  // Return an array with time copies of str
-  
-  var strings = [];
-  for (var i = 0; i < time; i++) {
-    strings.push(str);
+});
+doc.addEventListener('keyup', function (e) {
+  if (e.keyCode === 32) {
+    stopThisShit();
   }
-  return strings;
-}
-
-function assemble(statements) {
-  var slices = [];
-  
-  statements.forEach(function (stmt) {
-    var sliced = slicer(stmt);
-    var interstatementPause = pause("", 3);
-    slices = slices.concat(sliced);
-    
-    // Pause on the completed statement
-    slices = slices.concat(pause(stmt, 30));
-    
-    // Backspace it away
-    // slices = slices.concat(sliced.reverse());
-    slices = slices.concat(interstatementPause);
-  });
-  return slices;
-}
-
-var statements = [
-  "Knock, Knock, Lu...",
-  "Happy Birthday to you!",
-  "Wish the best of luck to you...",
-  ";)"
-]
-
-function rand(min, max) {
-  return min + Math.random() * (max - min) | 0;
-}
-
-var slices = assemble(statements);
-
-function animate() {
-  var current = slices.shift();
-  set(current);
-  slices.push(current);
-  setTimeout(animate, rand(50, 250));
-}
-
-animate()
-
-// Get canvas
-var cvs = document.getElementById('cvs');
-
-// Canvas fills window
-cvs.height = window.innerHeight;
-cvs.width = window.innerWidth;
-
-// Get canvas context
-var ctx = cvs.getContext('2d');
-
-// Set font, size & number of columns
-var font = 'arial';
-var fontSize = 18;
-ctx.font = fontSize + 'px ' + font;
-var cols = cvs.width / fontSize;
-
-// Characters
-var charSet;
-charSet = 'mimilo4ka'; // Hex
-charSet = charSet.split(''); // Convert string to array
-
-// One drop per column, row set randomly
-var drops = [];
-for (var col = 0; col < cols; col++)
-  drops[col] = Math.floor(Math.random() * cvs.height);
-
-// Call rain() every 25ms
-setInterval(rain, 25);
-
-function rain() {
-  // Background, black, translucent
-  /* This is key to the fade effect, clear the window with an alpha of 0.05, which doesn't clear it entirely but leaves existing text progressively dimmed with each subsequent call to rain()
-   */
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-  ctx.fillRect(0, 0, cvs.width, cvs.height);
-  
-  // For each column / drop
-  for (var col = 0; col < drops.length; col++) {
-    // Pick a random char
-    var char = charSet[Math.floor(Math.random() * charSet.length)];
-    // Pick a random colour
-    ctx.fillStyle = randColour();
-    // Draw the char
-    ctx.fillText(char, col * fontSize, drops[col] * fontSize);
-    // Randomly reset drop back to top row
-    if (Math.random() > 0.99)
-      drops[col] = 0;
-
-    drops[col]++; // Move drop down a row
-  }
-}
-
-function randColour()
-{
-  return'rgb(' + 
-    Math.floor(Math.random() * 256) + ',' + 
-    Math.floor(Math.random() * 256) + ',' + 
-    Math.floor(Math.random() * 256) + ')';
-}
+});
+btn.addEventListener('mousedown', startThisShit);
+btn.addEventListener('mouseup', stopThisShit);
+range.addEventListener('change', function () {
+  frequency = 1000 / range.value;
+});
